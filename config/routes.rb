80964908fb42bf9f2117dc6b623538e3aa0be69a
mkaram538 <mkaram538@gmail.com>
2021-10-04 02:52:18 -0500

    CS-431 : Adding users and updating front end

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-  resources :rsvps
   resources :events do
     resources :rsvps 
   end
 
-  root "events#index"
+  root "users#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_04_021117) do
+ActiveRecord::Schema.define(version: 2021_10_04_060315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 2021_10_04_021117) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_rsvps_on_event_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "f_name"
+    t.string "l_name"
+    t.string "email"
+    t.boolean "isAdmin"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "rsvps", "events"
