14a12cc7f4929b0089688df8eeeaddf3823b3e56
James Streets <james.m.streets@gmail.com>
2021-10-19 10:23:08 -0500

    Seeded two events and one rsvp into db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+event = Event.create([
+  {
+    event_name: 'Sample Event',
+    event_date: '10-08-2021',
+    description: 'Social event',
+    event_start_time: '12:40 pm',
+    event_end_time: '1:40 pm'
+  },
+  {
+    event_name: 'Sample Event 2',
+    event_date: '12-12-2021',
+    description: 'Corporate event',
+    event_start_time: '1:30 pm',
+    event_end_time: '2:00 pm'
+  },
+])
+
+rsvp = Rsvp.create([
+  {
+    event_name: 'Sample Event',
+    event_date: '10-08-2021',
+    f_name: 'James',
+    l_name: 'Streets',
+    email: 'abc@gamil.com',
+    event: event.first
+  }
+])
