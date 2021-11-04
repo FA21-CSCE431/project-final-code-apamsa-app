7bb4de4706e6944661b08fb31300e9b882a5ddae
James Streets <james.m.streets@gmail.com>
2021-11-04 10:50:37 -0500

    Fixed import mui icon module

--- app/javascript/components/BlogPage/blogPost.js
+++ app/javascript/components/BlogPage/blogPost.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from 'styled-components'
-import { TextField, Button, Stack } from "@mui/material";
-import SendIcon from '@mui/icons-material';
+import { TextField, Button, Stack, SendIcon } from "@mui/material";
+// import SendIcon from '@mui/icons-material/Send';
 
 const Card = styled.div`
   border: 1px solid #efefef;
