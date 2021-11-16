ee46d49bc113beeb95a80b2b411368bcb36bb659
mkaram538 <mkaram538@tamu.edu>
2021-11-15 20:22:24 -0600

    frontend-dev : error change

--- app/javascript/components/EventsPage/Event.js
+++ app/javascript/components/EventsPage/Event.js
@@ -1,98 +1,173 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Event from "./Event";
+import {
+  CardActions,
+  CardMedia,
+  CardHeader,
+  Button,
+  CardContent,
+  Paper,
+  Typography,
+  IconButton,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  AlertTitle,
+  Alert,
+} from "@mui/material";
+
+import PlaceholdreImage from "../../../assets/images/apamsa.png";
+import BeenhereIcon from "@mui/icons-material/Beenhere";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CloseIcon from "@mui/icons-material/Close";
 import axios from "axios";
-import styled from "styled-components";
-import { Stack } from "@mui/material";
-import CreateEvent from "./createEvent";
-import { setEvents, incrementCount } from "../objects/event/eventsSlice";
 
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(1, 1fr);
-  grid-gap: 20px;
-  justify-items: center;
-  align-content: center;
-  width: 30%;
-  padding: 20px;
-  > div {
-    background-color: #fff;
-    border-radius: 5px;
-    padding: 20px;
-  }
-`;
+const Event = ({
+  event_id,
+  event_name,
+  event_date,
+  description,
+  event_start_time,
+  event_end_time,
+  slug,
+  img_url,
+  ...props
+}) => {
+  // const [rsvped, setRsvped] = useState(false);
+  const [openConfirmDelete, setOpenConfirmnDelete] = useState(false);
+  const [openDeleted, setOpenDeleted] = useState(false);
+  const [openRsvp, setOpenRsvp] = useState(false);
+  const is_admin = useSelector((state) => state.user.admin);
 
-const Events = () => {
-  // Calendar
-  const [dateValue, setdateValue] = useState(new Date());
-  const dispatch = useDispatch();
-
-  function onChange(nextValue) {
-    setdateValue(nextValue);
-  }
-
-  const events = useSelector((state) => state.events.currentEvents);
-
-  const getEvents = () => {
+  const handleRsvpClick = () => {
+    setOpenRsvp(true);
     axios
-      .get("api/v1/events.json")
-      .then((resp) => {
-        dispatch(setEvents(resp.data.data));
+      .post("/api/v1/rsvps", {
+        ["event_name"]: event_name,
+        ["event_date"]: event_date,
+        ["name"]: useSelector((state) => state.user.name),
+        ["email"]: useSelector((state) => state.user.email),
+        ["event"]: event_id,
       })
+      .then((resp) => console.log(resp))
       .catch((resp) => console.log(resp));
   };
 
-  const updates = useSelector((state) => state.events.updateCount);
+  const handleDeleteClick = () => {
+    setOpenConfirmnDelete(true);
+  };
 
-  useEffect(() => {
-    getEvents();
-  }, [updates]);
+  const handleConfirmDelete = () => {
+    const url = `/api/v1/events/${slug}`;
 
-  // Events layout
-  const grid = events.map((event, index) => {
-    const {
-      event_name,
-      event_date,
-      description,
-      event_start_time,
-      event_end_time,
-      slug,
-      img_url,
-    } = event.attributes;
+    axios
+      .delete(url)
+      .then((resp) => console.log(resp))
+      .catch((resp) => console.log(resp));
 
-    const event_id = event.id;
-
-    return (
-      <Event
-        key={index}
-        event_id={event_id}
-        event_name={event_name}
-        event_date={event_date}
-        description={description}
-        event_start_time={event_start_time}
-        event_end_time={event_end_time}
-        slug={slug}
-        img_url={img_url}
-      />
-    );
-  });
+    setOpenDeleted(true);
+    setOpenConfirmnDelete(false);
+  };
 
   return (
-    <div style={{ margin: "5px 15px 10px" }}>
-      <Stack
-        spacing={10}
-        direction="row"
-        justifyContent="space-evenly"
-        alignItems="stretch"
+    <div>
+      <Paper>
+        <CardMedia
+          component="img"
+          image={PlaceholdreImage}
+          alt="Event flyer 1"
+        />
+        <CardHeader title={event_name} subheader={event_date} />
+        <CardContent>
+          <Typography variant="subtitle1">
+            {event_start_time} to {event_end_time}
+          </Typography>
+          <br />
+          <Typography paragraph>{description}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="contained"
+            endIcon={<BeenhereIcon />}
+            onClick={handleRsvpClick}
+          >
+            RSVP Here
+          </Button>
+          {is_admin == true && (
+            <IconButton onClick={handleDeleteClick}>
+              <DeleteIcon />
+            </IconButton>
+          )}
+        </CardActions>
+      </Paper>
+
+      <Dialog
+        open={openConfirmDelete}
+        onClose={() => {
+          setOpenConfirmnDelete(false);
+        }}
       >
-        {/* Blog Posts */}
-        <Grid>
-          <CreateEvent /> {/* Only for ADMIN */}
-          {grid}
-        </Grid>
-      </Stack>
+        <DialogTitle>
+          {`Are you sure you want to delete ${event_name}`}
+        </DialogTitle>
+        <DialogActions>
+          <Button variant="contained" onClick={handleConfirmDelete}>
+            Yes
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              setOpenConfirmnDelete(false);
+            }}
+          >
+            No
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog
+        open={openDeleted}
+        onClose={() => {
+          setOpenDeleted(false);
+        }}
+      >
+        <DialogTitle id="alert-dialog-title">
+          <Alert>{event_name} was successfully deleted!</Alert>
+        </DialogTitle>
+        <DialogActions>
+          <IconButton
+            onClick={() => {
+              setOpenDeleted(false);
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </DialogActions>
+      </Dialog>
+      <Dialog
+        open={openRsvp}
+        onClose={() => {
+          setOpenRsvp(false);
+        }}
+      >
+        <DialogTitle>
+          <Alert severity="success">
+            <AlertTitle>Successful Rsvp</AlertTitle>
+            Your RSVP was successful -{" "}
+            <strong>Don't forget to add it to your calendar!</strong>
+          </Alert>
+        </DialogTitle>
+        <DialogActions>
+          <IconButton
+            onClick={() => {
+              setOpenRsvp(false);
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
 
-export default Events;
+export default Event;
