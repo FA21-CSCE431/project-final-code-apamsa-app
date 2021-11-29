3488277461b185ebd224d1c95dd36bf852cd50ae
mkaram538 <mkaram538@tamu.edu>
2021-11-29 03:06:26 -0600

    frontend-dev : filter by date  updates

--- app/javascript/components/objects/event/eventsSlice.js
+++ app/javascript/components/objects/event/eventsSlice.js
@@ -4,18 +4,43 @@ export const eventsSlice = createSlice({
   name: "events",
   initialState: {
     currentEvents: [],
+    allEvents: [],
+    filterDate: "",
     updateCount: 0,
+    newEventDate: new Date(),
   },
   reducers: {
     setEvents: (state, action) => {
+      state.allEvents = action.payload;
       state.currentEvents = action.payload;
     },
     incrementCount: (state) => {
       state.updateCount++;
     },
+    setDate: (state, action) => {
+      state.filterDate = action.payload;
+    },
+    filterByDate: (state) => {
+      state.currentEvents = state.allEvents.filter(
+        (event) => event.attributes.event_date == state.filterDate
+      );
+    },
+    resetFilter: (state) => {
+      state.currentEvents = state.allEvents;
+    },
+    updateNewEventDate: (state, payload) => {
+      state.newEventDate = action.payload;
+    },
   },
 });
 
-export const { setEvents, incrementCount } = eventsSlice.actions;
+export const {
+  setEvents,
+  incrementCount,
+  setDate,
+  filterByDate,
+  resetFilter,
+  updateNewEventDate,
+} = eventsSlice.actions;
 
 export default eventsSlice.reducer;
